6825dd1c2b030184671e79381df166566d7c0492
Anuj kumar M <146652972+anujkumar-m@users.noreply.github.com>
2025-06-11 11:56:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def maxProductDifference(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        num=sorted(nums)
+        return num
