9d7d4fd0e3d24231d8efa606fc7b0d309db8318c
Anuj kumar M <146652972+anujkumar-m@users.noreply.github.com>
2025-07-24 14:55:53 +0530

    Time: 1 ms (45.45%), Space: 12.6 MB (9.92%) - LeetHub

---
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def replaceDigits(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        s=list(s)
+        for i in range(1,len(s),2):
+            s[i]=chr(ord(s[i-1])+int(s[i]))
+        return ''.join(s)
