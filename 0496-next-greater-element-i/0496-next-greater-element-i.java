4a3f38770f1de080e7d1b69222294d3a3a7a242b
Anuj kumar M <146652972+anujkumar-m@users.noreply.github.com>
2025-08-22 11:49:27 +0530

    Time: 3 ms (91.64%), Space: 44.4 MB (26.11%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,15 +1,17 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] arr=new int[nums1.length];
-        for(int i=0;i<nums1.length;i++){
-            arr[i]=-1;
-            boolean f=false;
-            for(int j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j]) {f=true;}
-                if(f && nums2[j]>nums1[i]) {arr[i]=nums2[j];
-                break;}
+        HashMap<Integer,Integer> hm=new HashMap();
+        Stack<Integer> s=new Stack();
+        int[] res=new int[nums1.length];
+        for(int ele:nums2){
+            while(!s.isEmpty() && s.peek()<ele){
+                hm.put(s.pop(),ele);
             }
+            s.push(ele);
         }
-        return arr;
+        for(int i=0;i<nums1.length;i++){
+            res[i]=hm.containsKey(nums1[i])?hm.get(nums1[i]):-1;
+        }
+        return res;
     }
 }
