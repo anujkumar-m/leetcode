10d23a919e65a3dc4e164ff4b5e82bd753e47064
Anuj kumar M <146652972+anujkumar-m@users.noreply.github.com>
2025-08-22 11:55:51 +0530

    Time: 5 ms (97.25%), Space: 46.4 MB (49.62%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,20 +1,13 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int n = nums.length;
-        int []res = new int[n];
-        int []arr = new int[n*2];
-        for(int i = 0; i < n; i++)
-            arr[i] = arr[i+n] = nums[i];
-        for(int i = 0; i < n; i++){
-            res[i] = -1;
-            for(int j = i+1; j < n*2; j++){
-                if (arr[j] > nums[i]){
-                    res[i] = arr[j];
-                    break;
-                }
-            }
+        Deque<Integer> s=new ArrayDeque();
+        int[] res=new int[nums.length];
+        Arrays.fill(res,-1);
+        for(int i=0;i<2*nums.length;i++){
+            while(!s.isEmpty() && nums[s.peek()]<nums[i%nums.length])
+                res[s.pop()]=nums[i%nums.length];
+        s.push(i%nums.length);
         }
-
         return res;
     }
 }
