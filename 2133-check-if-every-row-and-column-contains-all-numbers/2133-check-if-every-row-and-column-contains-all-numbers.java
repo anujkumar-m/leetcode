dec8388c13fa8c7d71358d7c623244076a0f8022
Anuj kumar M <146652972+anujkumar-m@users.noreply.github.com>
2025-09-25 21:21:05 +0530

    Time: 26 ms (35.65%), Space: 45.5 MB (49.51%) - LeetHub

---
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkValid(int[][] matrix) {
+        for(int i=0;i<matrix.length;i++){
+            Set<Integer> row = new HashSet<>();
+            Set<Integer> col = new HashSet<>();
+            for(int j=0;j<matrix[0].length;j++){
+               if(!row.add(matrix[i][j]) || !col.add(matrix[j][i])) return false;
+        }
+    }
+        return true;
+    }
+}
