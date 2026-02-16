52e5794e761843ad53a59fdfb6b8ee18dc94d25b
Anuj kumar M <anujkumar72716@gmail.com>
2026-02-16 09:31:59 +0530

    Time: 1 ms (60.34%), Space: 41.9 MB (99.17%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,19 @@
+// class Solution {
+//     public int reverseBits(int n) {
+//         return Integer.reverse(n);
+//     }   
+// }
+
+class Solution {
+    public int reverseBits(int n) {
+        for(int i = 0 ;i < 16 ; i++){
+            int left = (n>>i)&1;
+            int right = (n >> (31 - i)&1);
+            if(left != right){
+                n^=1<<i;
+                n^=1<<(31-i);
+            }
+        }
+        return n;
+    }   
+}
