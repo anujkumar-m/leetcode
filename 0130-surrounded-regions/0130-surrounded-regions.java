4c312b1c33061f497951b21edd951d3ef3078e63
Anuj kumar M <anujkumar72716@gmail.com>
2026-02-23 15:59:08 +0530

    Time: 3 ms (31.68%), Space: 47.9 MB (56.58%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,31 +1,26 @@
 class Solution {
-    
-        public void dfs(int i,int j,char[][] grid){
-            if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]!='O') return;
+    public void dfs(int i, int j ,char[][] grid){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j]!='O') return;
+        grid[i][j] = '*';
+        dfs(i-1,j,grid);
+        dfs(i,j+1,grid);
+        dfs(i+1,j,grid);
+        dfs(i,j-1,grid);
+    }
 
-            grid[i][j]='*';
-            dfs(i-1,j,grid);
-            dfs(i+1,j,grid);
-            dfs(i,j-1,grid);
-            dfs(i,j+1,grid);
-        }
-       public void solve(char[][] board) {
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(i==0 || j==0 || i==board.length-1 || j==board[0].length-1){
-                    if(board[i][j]=='O') dfs(i,j,board);
-
+    public void solve(char[][] board) {
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0 ; j < board[0].length;j++){
+                if(i == 0 || i == board.length - 1 || j == 0 || j == board[0].length -1){
+                    if(board[i][j] == 'O') dfs(i, j , board);
                 }
             }
         }
-         for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-
-                    if(board[i][j]=='O') board[i][j]='X';
-                    else if(board[i][j]=='*') board[i][j]='O';
+         for(int i = 0; i < board.length; i++){
+            for(int j = 0 ; j < board[0].length;j++){
+            if(board[i][j] == 'O') board[i][j] = 'X';
+            else if(board[i][j] == '*') board[i][j] = 'O';
             }
-
         }
-
     }
 }
