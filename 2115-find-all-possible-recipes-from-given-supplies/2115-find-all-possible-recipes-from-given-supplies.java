2c5c48c5260af433486a537bfe7826335bf504c6
Anuj kumar M <anujkumar72716@gmail.com>
2026-02-25 16:15:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
@@ -0,0 +1,7 @@
+class Solution {
+    public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+        
+    List<String> list = Arrays.asList(recipes);
+    return list;
+    }
+}
