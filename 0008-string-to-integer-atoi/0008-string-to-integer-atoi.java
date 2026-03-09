ae496e4c635d5038727032730fa53101ad0cc051
Anuj kumar M <anujkumar72716@gmail.com>
2026-03-09 14:59:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int myAtoi(String s) {
+       s= s.trim();
+        int i = 0;
+        int sign = 1;
+        if(s.charAt(0) == '+' || s.charAt(0) == '-'){
+            sign=(s.charAt(0) == '+')? 1 :-1;
+            i++;
+        }
+        String num = "0123456789";
+        long n = 0;
+        while(i < s.length() && num.contains(String.valueOf(s.charAt(i)))){
+            n = n * 10 + (s.charAt(i) - '0');
+            if(sign == 1 && n > Integer.MAX_VALUE)
+                return Integer.MAX_VALUE;
+
+            if(sign == -1 && -n < Integer.MIN_VALUE)
+                return Integer.MIN_VALUE;
+            i++;
+        }
+
+        return (int)(n * sign);
+    }
+}
