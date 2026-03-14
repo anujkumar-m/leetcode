3ad27f81e193d6e7262e767482bb015759e9f828
Anuj kumar M <anujkumar72716@gmail.com>
2026-03-14 11:03:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,29 +1,34 @@
 class Solution {
-    int findminindx(int[] nums){
-        int n=nums.length-1;
-        int left=0,right=n;
-        while(left<right){
-           int mid = (left+right)/2;
-            if(nums[mid]>nums[right]) left=mid+1;
-            else right =mid;
-        }
-        return left;
-    }
-    int bs(int[] nums,int target,int left,int right){
-        while(left<=right){
-            int mid=(left+right)/2;
-            if(nums[mid]==target) return mid;
-            else if(target>nums[mid]) left=mid+1;
-            else right=mid-1;
-        }
-        return -1;
-    }
+    public int search(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
 
-    public int search(int[] nums, int target) {
-        int n=nums.length;
-        int minidx=findminindx(nums);
-        int ans = bs(nums,target,minidx,n-1);
-        if(ans!=-1)return ans;
-        return bs(nums,target,0,minidx-1);
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+
+            if (arr[low] <= arr[mid]) {
+
+                if (target >= arr[low] && target < arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+
+            else {
+
+                if (target > arr[mid] && target <= arr[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
     }
 }
