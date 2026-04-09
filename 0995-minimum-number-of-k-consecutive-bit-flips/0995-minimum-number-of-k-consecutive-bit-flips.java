d9cbe2dc1b14debdf08f4b35e78632a587e569ca
Anuj kumar M <anujkumar72716@gmail.com>
2026-04-09 15:46:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.java
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.java
@@ -1,34 +1,25 @@
 class Solution {
-    public static int minKBitFlips(int[] arr, int k) {
-        int n = arr.length;
+    public int minKBitFlips(int[] nums, int k) {
         Queue<Integer> q = new LinkedList<>();
-        int flips = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            // Remove expired flips (out of window)
-            if (!q.isEmpty() && q.peek() < i - k + 1) {
+        int flip = 0;
+        int n = nums.length;
+        for(int i =0; i < n; i++){
+            
+            if(!q.isEmpty() && q.peek() < i - k + 1){
                 q.poll();
             }
-
-            // Get current value after flips
-            int current = arr[i];
-            if (q.size() % 2 != 0) {
-                current = 1 - current;
+            int curr = nums[i];
+            if(q.size() % 2 != 0){
+                curr = 1 - curr;
             }
 
-            // If still 0 → must flip
-            if (current == 0) {
-                if (i + k > n) {
-                    return -1;
-                }
+            if(curr == 0){
+                if(i+k > n) return -1;
 
-                q.offer(i); // start flip at index i
-                flips++;
+                q.offer(i);
+                flip++;
             }
         }
-
-        return flips;
+        return flip;
     }
-
 }
