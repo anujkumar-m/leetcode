70968711b085f519b99071e188cde52354a2ac50
Anuj kumar M <anujkumar72716@gmail.com>
2026-05-29 09:32:02 +0530

    Time: 2 ms (12.37%), Space: 43.6 MB (86.89%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,25 +1,26 @@
 class Solution {
     public int myAtoi(String s) {
-       s= s.trim();
+        s = s.trim();
         int i = 0;
         int sign = 1;
-        if(s.length() == 0) return 0;
-        if(s.charAt(i) == '+' || s.charAt(i) == '-'){
-            sign=(s.charAt(0) == '+')? 1 :-1;
+        if (s.length() == 0)
+            return 0;
+        if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+            sign = (s.charAt(0) == '+') ? 1 : -1;
             i++;
         }
         String num = "0123456789";
         long n = 0;
-        while(i < s.length() && num.contains(String.valueOf(s.charAt(i)))){
+        while (i < s.length() && num.contains(String.valueOf(s.charAt(i)))) {
             n = n * 10 + (s.charAt(i) - '0');
-            if(sign == 1 && n > Integer.MAX_VALUE)
+            if (sign == 1 && n > Integer.MAX_VALUE)
                 return Integer.MAX_VALUE;
 
-            if(sign == -1 && -n < Integer.MIN_VALUE)
+            if (sign == -1 && -n < Integer.MIN_VALUE)
                 return Integer.MIN_VALUE;
             i++;
         }
 
-        return (int)(n * sign);
+        return (int) (n * sign);
     }
 }
