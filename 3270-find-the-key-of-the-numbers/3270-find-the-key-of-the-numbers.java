882655ab9f179fd6eb49c79dec76889a466dbaf3
Anuj kumar M <anujkumar72716@gmail.com>
2026-08-13 16:01:23 +0530

    Time: 1 ms (98.8%), Space: 42.7 MB (61.72%) - LeetHub

--- 3270-find-the-key-of-the-numbers/3270-find-the-key-of-the-numbers.java
+++ 3270-find-the-key-of-the-numbers/3270-find-the-key-of-the-numbers.java
@@ -1,12 +1,19 @@
 class Solution {
     public int generateKey(int num1, int num2, int num3) {
-        StringBuilder s1 = new StringBuilder(String.format("%04d", num1));
-        StringBuilder s2 = new StringBuilder(String.format("%04d", num2));
-        StringBuilder s3 = new StringBuilder(String.format("%04d", num3));
-        StringBuilder s = new StringBuilder();
-        for(int i = 0; i < 4; i++){
-            s.append(Math.min(s1.charAt(i) - '0', Math.min(s2.charAt(i) - '0', s3.charAt(i) - '0')));
+        int ans = 0;
+        int p = 1;
+        for (int i = 0; i < 4; i++) {
+            int d1 = num1 % 10;
+            int d2 = num2 % 10;
+            int d3 = num3 % 10;
+
+            int min = Math.min(d1, Math.min(d2, d3));
+            ans += min * p;
+            num1 /= 10;
+            num2 /= 10;
+            num3 /= 10;
+            p *= 10;
         }
-        return Integer.parseInt(new String(s));
+        return ans;
     }
 }
