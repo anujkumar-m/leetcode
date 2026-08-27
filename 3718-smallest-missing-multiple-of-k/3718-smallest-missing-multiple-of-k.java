1a441adac809f3138830676cb4af972b9973213b
Anuj kumar M <anujkumar72716@gmail.com>
2026-08-27 11:46:54 +0530

    Time: 1 ms (88.41%), Space: 44.8 MB (89.46%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -1,22 +1,19 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
-        TreeSet<Integer> set = new TreeSet<>();
-        for (int n : nums) {
-            if (n % k == 0) {
-                set.add(n);
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (num % k == 0) {
+                set.add(num);
             }
         }
-        
-        if (set.isEmpty())
-            return k;
-        if (set.first() != k)
-            return k;
 
-        int first = set.first();
-        for (int i = 0; i < set.size(); i++) {
-            if (!set.contains(first * (i + 1)))
-                return first * (i + 1);
+        int multiple = k;
+
+        while (set.contains(multiple)) {
+            multiple += k;
         }
-        return first * (set.size() + 1);
+
+        return multiple;
     }
 }
